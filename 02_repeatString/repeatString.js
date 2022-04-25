01864915c525d301e87c67aa1df9e0e9dc5e6286
kadde92 <kadir.wafin@gmail.com>
2022-04-25 19:42:30 +0300

    did exc2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        let combo = '';
+        for (let i = 0; i < num; i++) {
+            combo += string;
+        }
+        return combo};
 };
 
 // Do not edit below this line
