7a104d6d6ea090395704cb8c62986f7d2cfa04dc
kadde92 <kadir.wafin@gmail.com>
2022-04-26 17:07:56 +0300

    Did exc5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+    if (typeof (numOne, numTwo) !== 'number' || numOne < 0 || numTwo < 0) {
+        return 'ERROR'
+    } else {
+        let total = 0;
+        let min = (Math.min(numOne, numTwo))
+        let max = (Math.max(numOne, numTwo))
+        for (let i = min; i <= max; i++) {
+            total += i;
+        }
+        return total
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
