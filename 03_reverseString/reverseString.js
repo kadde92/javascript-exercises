8c0841b3ec5723e3405045230e4cd874c453ccc6
kadde92 <kadir.wafin@gmail.com>
2022-04-26 13:47:15 +0300

    Did exc3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseString;
+    const arr = word.split("")
+    arr.reverse()
+    let compound = "";
+    for (let i = 0; i < arr.length; i++) {
+        compound+=arr[i];
+        
+    } 
+    return compound;
 };
 
 // Do not edit below this line
