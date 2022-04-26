6f1e5e019dd9e388bf3105fb53a988d044e3fb45
kadde92 <kadir.wafin@gmail.com>
2022-04-26 15:47:54 +0300

    did exc4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,41 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, remove, ...args) {
+    if (array.indexOf(remove) !== -1 && args === []) {
+        let index = array.indexOf(remove)
+        array.splice(index, 1);
+        return array;
+    } else if (array.indexOf(remove) === -1 && args === []) {
+        return array;
+    }
 
-};
+    if (array.indexOf(remove) !== -1 && args !== []) {
+        let index = array.indexOf(remove)
+        array.splice(index, 1);
+        for (const argsIndex of args) {
+            let toRemove = array.indexOf(argsIndex)
+            if (array.indexOf(argsIndex) !== -1) {
+                array.splice(toRemove, 1)
+            }
+        } return array;
+
+    } else if (array.indexOf(remove) === -1 && args !== []) {
+        for (const argsIndex of args) {
+            let toRemove = array.indexOf(argsIndex)
+            if (array.indexOf(argsIndex) !== -1) {
+                array.splice(toRemove, 1)
+            }
+        } return array;
+    }
+}
+
+
+
+
+// let index = array.indexOf(remove)
+//     array.splice(index,1);
+//     return array;
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
