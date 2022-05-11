dba3c4fa918e4ad38872c2eac5cf566a897666d9
kadde92 <kadir.wafin@gmail.com>
2022-05-11 13:57:09 +0300

    Solved Palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punc = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const cleanPunc = string
+        .replace(punc, '')
+        .toLowerCase()
+        .replaceAll(' ', '')
+    return ([...cleanPunc].reverse().toString() === [...cleanPunc].toString() ? true : false)
 };
+
+// ensin muuta sana array -> poista välit ja punctutation, myöskin string array pitää muuntaa toLowerCase() vetailtaessa
+//
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
