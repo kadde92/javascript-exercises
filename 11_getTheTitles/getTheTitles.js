f97453e59b7c7caa40266a428d8651e72e803e3c
kadde92 <kadir.wafin@gmail.com>
2022-05-11 19:15:11 +0300

    Titles function solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    const bookTitles = books.map(n => n.title);
+    return bookTitles
+};    
 
 // Do not edit below this line
 module.exports = getTheTitles;
